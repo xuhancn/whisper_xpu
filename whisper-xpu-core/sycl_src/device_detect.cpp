af8d9295488758142871cefb374ad046f50ef494
Xu Han <xu.han@outlook.com>
2026-07-26 14:51:48 +0800

    Fix SYCL device index mismatch with try-catch

--- whisper-xpu-core/sycl_src/device_detect.cpp
+++ whisper-xpu-core/sycl_src/device_detect.cpp
@@ -55,7 +55,9 @@ std::vector<DeviceInfo> get_available_devices() {
                 inf.vendor = d.get_info<sycl::info::device::vendor>();
                 inf.compute_units = (int)d.get_info<sycl::info::device::max_compute_units>();
                 inf.total_mem = d.get_info<sycl::info::device::global_mem_size>();
-                size_t f = 0, t = 0; ggml_backend_sycl_get_device_memory(idx, &f, &t); inf.free_mem = f;
+                size_t f = 0, t = 0;
+                try { ggml_backend_sycl_get_device_memory(idx, &f, &t); inf.free_mem = f; }
+                catch (...) { inf.free_mem = 0; }
                 list.push_back(inf); ++idx;
             }
     } catch (const std::exception &e) {
